0f104e89f9ccee0ab26cb6cfae1b433d46ca0cd2
Staffan Einarsson <staffan.einarsson@gmail.com>
2013-11-11 14:37:21 +0100

    Fixed a problem where the wrong coordinate constructor was being called.

--- src/ee_caret.erl
+++ src/ee_caret.erl
@@ -68,37 +68,37 @@ move_horizontally(Caret, #ee_buffer_coords{line_no = LineNo, line_offset = LineO
 	move_to_end_of_line(Caret, Buffer);
 %% Move left more than one line -> subtract offset and recurse on prev line
 move_horizontally(Caret, #ee_buffer_coords{line_no = LineNo, line_offset = LineOffset}, Amount, Buffer, _CurrentLineLength, PreviousLineLength, LastLineNo) when Amount < 1 - LineOffset ->
-	move_horizontally(Caret, ee_buffer:new_buffer_coords(LineNo - 1, PreviousLineLength + 1), Amount + LineOffset, Buffer, PreviousLineLength, ee_buffer:get_line_length(Buffer, LineNo - 2), LastLineNo);
+	move_horizontally(Caret, ee_buffer_coords:new(LineNo - 1, PreviousLineLength + 1), Amount + LineOffset, Buffer, PreviousLineLength, ee_buffer:get_line_length(Buffer, LineNo - 2), LastLineNo);
 %% Move right more than one line -> subtract offset and recurse on next line
 move_horizontally(Caret, #ee_buffer_coords{line_no = LineNo, line_offset = LineOffset}, Amount, Buffer, CurrentLineLength, _PreviousLineLength, LastLineNo) when Amount > CurrentLineLength + 1 - LineOffset ->
-	move_horizontally(Caret, ee_buffer:new_buffer_coords(LineNo + 1, 1), Amount - (CurrentLineLength + 1 - LineOffset) - 1, Buffer, ee_buffer:get_line_length(Buffer, LineNo + 1), CurrentLineLength, LastLineNo);
+	move_horizontally(Caret, ee_buffer_coords:new(LineNo + 1, 1), Amount - (CurrentLineLength + 1 - LineOffset) - 1, Buffer, ee_buffer:get_line_length(Buffer, LineNo + 1), CurrentLineLength, LastLineNo);
 %% Move within current line -> Update coords and caret
 move_horizontally(_Caret, #ee_buffer_coords{line_no = LineNo, line_offset = LineOffset}, Amount, Buffer, _CurrentLineLength, _PreviousLineLength, _LastLineNo) ->
-	buffer_coords_to_caret(ee_buffer:new_buffer_coords(LineNo, LineOffset + Amount), Buffer).
+	buffer_coords_to_caret(ee_buffer_coords:new(LineNo, LineOffset + Amount), Buffer).
 	
 move_to_beginning_of_line(#ee_caret{} = Caret, Buffer) ->
 	#ee_buffer_coords{line_no = LineNo} = caret_to_buffer_coords(Caret, Buffer),
-	buffer_coords_to_caret(ee_buffer:new_buffer_coords(LineNo, 1), Buffer).
+	buffer_coords_to_caret(ee_buffer_coords:new(LineNo, 1), Buffer).
 
 move_to_end_of_line(#ee_caret{} = Caret, Buffer) ->
 	#ee_buffer_coords{line_no = LineNo} = caret_to_buffer_coords(Caret, Buffer),
-	buffer_coords_to_caret(ee_buffer:new_buffer_coords(LineNo, ee_buffer:get_line_length(Buffer, LineNo) + 1), Buffer).
+	buffer_coords_to_caret(ee_buffer_coords:new(LineNo, ee_buffer:get_line_length(Buffer, LineNo) + 1), Buffer).
 
 move_to(#ee_caret{line_no = LineNo} = Caret, Buffer) ->
 	move_to(Caret, caret_to_buffer_coords(Caret, Buffer), Buffer, ee_buffer:get_line_length(Buffer, LineNo), ee_buffer:get_num_lines(Buffer)).
 
 %% Move to a line before the beginning -> Clamp to first line
 move_to(_Caret, #ee_buffer_coords{line_no = LineNo}, Buffer, _LineLength, _NumLines) when LineNo < 1 ->
-	buffer_coords_to_caret(ee_buffer:new_buffer_coords(1, 1), Buffer);
+	buffer_coords_to_caret(ee_buffer_coords:new(1, 1), Buffer);
 %% Move to a line after the end -> Clamp to last line
 move_to(_Caret, #ee_buffer_coords{line_no = LineNo}, Buffer, _LineLength, NumLines) when LineNo > NumLines ->
-	buffer_coords_to_caret(ee_buffer:new_buffer_coords(NumLines, ee_buffer:get_line_length(Buffer, NumLines) + 1), Buffer);
+	buffer_coords_to_caret(ee_buffer_coords:new(NumLines, ee_buffer:get_line_length(Buffer, NumLines) + 1), Buffer);
 %% Move to a line where the given pos is before beginning of line -> Clamp to beginning of line
 move_to(_Caret, #ee_buffer_coords{line_no = LineNo, line_offset = LineOffset}, Buffer, _LineLength, _NumLines) when LineOffset < 1 ->
-	buffer_coords_to_caret(ee_buffer:new_buffer_coords(LineNo, 1), Buffer);
+	buffer_coords_to_caret(ee_buffer_coords:new(LineNo, 1), Buffer);
 %% Move to a line where the given pos is after end of line -> Clamp to end of line
 move_to(_Caret, #ee_buffer_coords{line_no = LineNo, line_offset = LineOffset}, Buffer, LineLength, _NumLines) when LineOffset > LineLength + 1 ->
-	buffer_coords_to_caret(ee_buffer:new_buffer_coords(LineNo, LineLength + 1), Buffer);
+	buffer_coords_to_caret(ee_buffer_coords:new(LineNo, LineLength + 1), Buffer);
 %% Move to a line where the given pos is valid
 move_to(Caret, _Coords, _Buffer, _LineLength, _NumLines) ->
 	Caret.	
@@ -107,7 +107,7 @@ caret_to_buffer_coords(#ee_caret{line_no = LineNo, col_no = ColNo}, Buffer) ->
 	LineContents = ee_buffer:get_line_contents(ee_buffer:get_line(Buffer, LineNo)),
 	%% Get each char on line until colno has been reached.
 	Offset = caret_to_buffer_coords_loop(LineContents, 1, ColNo),
-	ee_buffer:new_buffer_coords(LineNo, Offset).
+	ee_buffer_coords:new(LineNo, Offset).
 
 caret_to_buffer_coords_loop(no_contents, _, _) ->
 	no_contents;
